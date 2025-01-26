2cb5b14d829ada15303e12a8b5576c0a19a8cca2
unknown <darshanjadhav775@gmail.com>
2025-01-26 23:24:22 +0530

    deploying frontend

--- frontend/src/components/chat_bot/config.jsx
+++ frontend/src/components/chat_bot/config.jsx
@@ -1,24 +1,24 @@
 import { createChatBotMessage } from 'react-chatbot-kit';
-import { Avatar } from './Component/Avatar.jsx';
-import { Header } from './Component/Avatar.jsx';
- import Options from './Options.jsx';
- import StartBtn from './Component/StartBtn.jsx';
+import { Avatar } from '../chat_bot/component/Avatar.jsx';
+import { Header } from '../chat_bot/component/Avatar.jsx';
+import Options from './Options.jsx';
+import StartBtn from './Component/StartBtn.jsx';
 
 const config = {
   botName: 'BUDDY BOT',
   initialMessages: [createChatBotMessage(`Welcome to BUDDY,click on start button to start!`, {
     widget: "startBtn"
-})],  
-  
+  })],
+
   customComponents: {
     header: (props) => <Header {...props} />,
-     botAvatar: (props) => <Avatar {...props} />,
+    botAvatar: (props) => <Avatar {...props} />,
   },
   widgets: [
     {
       widgetName: "startBtn",
       widgetFunc: (props) => <StartBtn {...props} />,
-  },
+    },
     {
       widgetName: "options",
       widgetFunc: (props) => <Options {...props} />,
